a1e8827f7234b78062fd525c9761e5e34da47b5f
prosper <edwardprosper001@gmail.com>
2023-01-09 10:19:31 +0100

    todo

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import TaskSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
 
-# Create your views here.
+
+@api_view(["GET"])
+def home(request):
+    if request.method == "GET":
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+        
+
